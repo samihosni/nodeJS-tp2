2ab66f5cd5fcb74c0ad39972775d9e2174735243
Sami Hosni <samy.hosni@gmail.com>
2024-06-01 12:13:32 +0100

    First commit

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const voiture=require('./routes/voiture')
+const mongoose=require('mongoose')
+const dotenv= require('dotenv')
 const PORT = 3000;
+dotenv.config()
+const MONGODB_URI=process.env.MONGODB_URI
 app.use(express.json());
 app.use('/voiture',voiture)
 
-
-app.listen(PORT, () => {
+mongoose.connect(MONGODB_URI).then(()=>{
+  console.log('Connected to MONGODB');
+  app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  })
+}).catch((err)=>{
+  console.error('Error connecting to MONGODB:',err.message)
+})
+
+
